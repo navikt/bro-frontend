c4ee5f7f60ccbce39c1ce47e78b34ed81d4ce9ad
Luan Tran <8613937+luantr@users.noreply.github.com>
2025-06-02 23:46:23 +0200

    add Nav dekoratoren

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,35 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
+import '@navikt/ds-css'
+import { fetchDecoratorReact } from '@navikt/nav-dekoratoren-moduler/ssr'
+
 import './globals.css'
+import { Page } from '@navikt/ds-react'
 
 export const metadata: Metadata = {
   title: 'Behovsrettet oppfølging',
   description: 'Skjema for behovsrettet oppfølging',
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+const RootLayout = async ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  const Decorator = await fetchDecoratorReact({
+    env: 'dev',
+  })
+
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="no">
+      <head>
+        <Decorator.HeadAssets />
+      </head>
+      <body>
+        <Page footer={<Decorator.Footer />}>
+          <Decorator.Header />
+          {children}
+          <Decorator.Scripts loader={Script} />
+        </Page>
+      </body>
     </html>
   )
 }
+
+export default RootLayout
