44ecfc5c328118c0cbfd670f87c142a434c04b59
Luan Tran <8613937+luantr@users.noreply.github.com>
2025-10-23 12:38:47 +0200

    add authz

---
+++ src/auth/rsc.ts
@@ -0,0 +1,31 @@
+'use server'
+
+import { logger } from '@navikt/next-logger'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { getToken, validateToken } from '@navikt/oasis'
+import { isLocalOrDemo } from '@/constants/envs'
+import { BASE_PATH } from '../../next.config'
+
+export async function verifyUserLoggedIn(): Promise<string> {
+  if (isLocalOrDemo) {
+    logger.warn('Is running locally, skipping RSC auth')
+    return 'fake-local-token'
+  }
+
+  const requestHeaders = await headers()
+  const token = getToken(requestHeaders)
+
+  if (!token) {
+    logger.info('Found no token, redirecting to login')
+    redirect(`/oauth2/login?redirect=${BASE_PATH}`)
+  }
+
+  const validationResult = await validateToken(token)
+  if (!validationResult.ok) {
+    logger.info('Invalid token, redirecting to login')
+    redirect(`/oauth2/login?redirect=${BASE_PATH}`)
+  }
+
+  return token
+}
---
+++ src/auth/tokenUtils.ts
@@ -0,0 +1,21 @@
+import { requestOboToken } from '@navikt/oasis'
+
+import { getServerEnv } from '@/constants/envs'
+
+export async function exchangeIdportenTokenForMeroppfolgingBackendTokenx(
+  idportenToken: string | null,
+): Promise<string> {
+  if (!idportenToken) {
+    throw new Error('Mangler idportenToken')
+  }
+
+  const MEROPPFOLGING_BACKEND_CLIENT_ID = `${getServerEnv().NAIS_CLUSTER_NAME}:team-esyfo:meroppfolging-backend`
+
+  const tokenxGrant = await requestOboToken(idportenToken, MEROPPFOLGING_BACKEND_CLIENT_ID)
+
+  if (!tokenxGrant.ok) {
+    throw new Error(`Failed to exchange idporten token for meroppfolging-backend tokenx: ${tokenxGrant.error}`)
+  }
+
+  return tokenxGrant.token
+}
