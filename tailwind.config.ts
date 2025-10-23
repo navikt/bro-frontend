5929a6ce8c959266e8f9adf354399a4d17ae672b
Luan Tran <8613937+luantr@users.noreply.github.com>
2025-10-23 12:36:13 +0200

    add packages and tailwind-config

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'tailwindcss'
-import navikt from '@navikt/ds-tailwind'
-
-const config: Config = {
-  presets: [navikt],
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  plugins: [],
-}
-export default config
