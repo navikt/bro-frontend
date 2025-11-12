1db5b8b5a643ea1a126a1e64f9e7a5dadc750ac6
Luan Tran <8613937+luantr@users.noreply.github.com>
2025-11-12 10:20:47 +0100

    wip

--- src/features/kartleggingssporsmal/summary/KartleggingssporsmalFormSummaryPage.tsx
+++ src/features/kartleggingssporsmal/summary/KartleggingssporsmalFormSummaryPage.tsx
@@ -7,6 +7,7 @@ import ThankYouAlert from '@/features/kartleggingssporsmal/summary/ThankYouAlert
 import { UsefulLinks } from '@/features/kartleggingssporsmal/summary/UsefulLinks'
 import { KartleggingssporsmalFormResponse } from '@/services/meroppfolging/schemas/formSnapshotSchema'
 import { Flexjar } from '@/components/flexjar/flexjar'
+import UxSignalsPanel from '@/components/ux-signals/UxSignalsPanel'
 
 type Props = {
   formResponse: KartleggingssporsmalFormResponse
@@ -20,11 +21,17 @@ export default function KartleggingssporsmalFormSummaryPage({ formResponse }: Pr
       <Heading size={'large'} level="1">
         Kartlegging av din situasjon
       </Heading>
+
       <ThankYouAlert date={formResponse.createdAt} />
+
       <BodyShort className="" spacing>
         Svarene dine gir Nav innsikt i hvordan vi skal følge deg opp fremover. Om vi ser behovet for tettere oppfølging
         enn det arbeidsgiveren din skal gi deg, vil du bli kontaktet av en Nav-veileder.
       </BodyShort>
+
+      <KartleggingssporsmalFormSummary items={summaryItems} />
+
+      <UxSignalsPanel />
 
       <UsefulLinks />
 
@@ -38,10 +45,6 @@ export default function KartleggingssporsmalFormSummaryPage({ formResponse }: Pr
         </Link>{' '}
         (åpner i ny fane) hvis det skulle være noe du lurer på.
       </BodyShort>
-
-      <KartleggingssporsmalFormSummary items={summaryItems} />
-
-      <Flexjar />
     </VStack>
   )
 }
