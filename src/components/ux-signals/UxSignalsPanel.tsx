5bb0e724bcd4d3a04e9898a38e7b720b4d5e4d40
Luan Tran <8613937+luantr@users.noreply.github.com>
2025-12-01 14:03:45 +0100

    add demo mode for ux-signals

--- src/components/ux-signals/UxSignalsPanel.tsx
+++ src/components/ux-signals/UxSignalsPanel.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import useUxSignalsScript from '@/hooks/uxSignals'
+import { isLocalOrDemo } from '@/env-variables/envHelpers'
 
 export default function UxSignalsPanel() {
   useUxSignalsScript(true)
 
-  return <div data-uxsignals-embed="panel-emwigf91dw" />
+  return <div data-uxsignals-embed="panel-emwigf91dw" data-uxsignals-mode={isLocalOrDemo ? 'demo' : ''} />
 }
