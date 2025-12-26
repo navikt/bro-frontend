ddf785e7608d28692d899b91d9bf7b36d6ef36c7
Audun Sørheim <audun.sorheim@nav.no>
2025-12-26 15:05:11 +0100

    wip

--- src/components/flexjar/survey.ts
+++ src/components/flexjar/survey.ts
@@ -1,17 +1,23 @@
-import { type FlexJarSurveyConfig } from '@navikt/flexjar-widget'
+import { createRatingSurvey } from '@navikt/flexjar-widget'
 
-export const survey: FlexJarSurveyConfig = {
-  rating: {
-    type: 'rating',
-    prompt: 'Hvordan opplevde du å svare på spørsmålene?',
-    description: 'Svarene du sender inn er anonyme, og blir brukt til videreutvikling av tjenesten.',
-  },
-  mainQuestion: {
-    type: 'text',
-    maxLength: 500,
-    minRows: 4,
-    prompt: 'Legg gjerne til en begrunnelse (valgfritt)',
-    description: 'Alle tilbakemeldinger er til stor nytte for oss',
-    required: false,
-  },
-}
+// export const survey: FlexJarSurveyConfig = {
+//   rating: {
+//     type: 'rating',
+//     prompt: 'Hvordan opplevde du å svare på spørsmålene?',
+//     description: 'Svarene du sender inn er anonyme, og blir brukt til videreutvikling av tjenesten.',
+//   },
+//   mainQuestion: {
+//     type: 'text',
+//     maxLength: 500,
+//     minRows: 4,
+//     prompt: 'Legg gjerne til en begrunnelse (valgfritt)',
+//     description: 'Alle tilbakemeldinger er til stor nytte for oss',
+//     required: false,
+//   },
+// }
+
+export const survey = createRatingSurvey({
+  ratingPrompt: "Hvordan opplevde du å svare på spørsmålene?'",
+  ratingDescription: 'Svarene du sender inn er anonyme, og blir brukt til videreutvikling av tjenesten.',
+  textPrompt: "Legg gjerne til en begrunnelse (valgfritt)'?",
+})
