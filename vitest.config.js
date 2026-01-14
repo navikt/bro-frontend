cc00c1aadfd21abb0f726e0fa2c9d69fa9fc14e2
André Wittwer <andre.wittwer@nav.no>
2026-01-14 11:43:56 +0100

    chore: add vitest env vars

--- vitest.config.js
+++ vitest.config.js
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    setupFiles: './vitest.setup.ts',
     include: ['src/**/*.{test,spec}.{ts,tsx}'],
   },
 })
---
+++ vitest.setup.ts
@@ -0,0 +1,2 @@
+process.env.NEXT_PUBLIC_RUNTIME_ENVIRONMENT = 'test'
+process.env.NEXT_PUBLIC_BASE_PATH = '/syk/kartleggingssporsmal'
