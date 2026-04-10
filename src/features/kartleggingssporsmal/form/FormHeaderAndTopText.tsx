527b5d5bd78b4f09d56bbd41dd0b451e89288820
Håkon Salomonsen Møller <hakon.salomonsen.moller@nav.no>
2026-04-10 10:58:18 +0200

    Adjust second paragraph above form

--- src/features/kartleggingssporsmal/form/FormHeaderAndTopText.tsx
+++ src/features/kartleggingssporsmal/form/FormHeaderAndTopText.tsx
@@ -14,7 +14,9 @@ export function FormHeaderAndTopText() {
         skal få fra arbeidsgiveren din.
       </BodyShort>
 
-      <BodyShort spacing>Svarene dine blir kun delt med Nav.</BodyShort>
+      <BodyShort spacing>
+        Svarene blir kun synlige for Nav, og deles ikke med din arbeidsgiver.
+      </BodyShort>
     </>
   );
 }
