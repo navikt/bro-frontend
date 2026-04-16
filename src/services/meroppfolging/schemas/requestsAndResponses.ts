3297f281985d97cbe074d2fb8bb0ae68f865ec7e
Håkon Salomonsen Møller <hakon.salomonsen.moller@nav.no>
2026-04-16 10:30:42 +0200

    Fix zod import

--- src/services/meroppfolging/schemas/requestsAndResponses.ts
+++ src/services/meroppfolging/schemas/requestsAndResponses.ts
@@ -1,4 +1,4 @@
-import { z } from "zod/v4";
+import { z } from "zod";
 import { formVariantSchema } from "@/forms/kartleggingssporsmal/formVariants/types/FormVariant";
 import { formSnapshotSchema } from "./formSnapshotSchema";
 
