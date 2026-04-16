2a03ab6c6746235a406789801e6adbfbf4fdf9f1
Håkon Salomonsen Møller <hakon.salomonsen.moller@nav.no>
2026-04-16 12:55:08 +0200

    Clean up rendering and effect flow in useDemoFormVariant

--- src/components/DemoInfoCard/useDemoFormVariant.ts
+++ src/components/DemoInfoCard/useDemoFormVariant.ts
@@ -1,4 +1,5 @@
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useCallback, useEffect } from "react";
 import { DEMO_SKJEMAVARIANT_URL_PARAM_KEY } from "@/appConfig";
 import { isLocalOrDemo } from "@/env-variables/envHelpers";
 import {
@@ -24,35 +25,38 @@ export function useDemoFormVariantViaParamIfDemo(
 
   let activeFormVariant = formVariantIfNotInDemo;
 
-  if (!isLocalOrDemo) {
-    return { activeFormVariant, changeDemoFormVariantViaParam: () => {} };
-  }
-
   const demoVariantFromUrl = searchParams.get(DEMO_SKJEMAVARIANT_URL_PARAM_KEY);
-
   const parseVariantResult = formVariantSchema.safeParse(demoVariantFromUrl);
 
-  if (!parseVariantResult.success) {
-    changeDemoFormVariantViaParam(DEFAULT_DEMO_FORM_VARIANT, {
-      replaceUrl: true,
-    });
-  } else {
+  // Override form variant with the one from URL parameter if we're in a demo
+  // environment
+  if (parseVariantResult.success && isLocalOrDemo) {
     activeFormVariant = parseVariantResult.data;
   }
 
-  function changeDemoFormVariantViaParam(
-    variant: FormVariant,
-    options?: { replaceUrl?: boolean },
-  ) {
-    const params = new URLSearchParams(searchParams.toString());
-    params.set(DEMO_SKJEMAVARIANT_URL_PARAM_KEY, variant);
+  const changeDemoFormVariantViaParam = useCallback(
+    (variant: FormVariant, options?: { replaceUrl?: boolean }) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(DEMO_SKJEMAVARIANT_URL_PARAM_KEY, variant);
 
-    if (options?.replaceUrl) {
-      router.replace(`${pathname}?${params.toString()}`);
-    } else {
-      router.push(`${pathname}?${params.toString()}`);
+      if (options?.replaceUrl) {
+        router.replace(`${pathname}?${params.toString()}`);
+      } else {
+        router.push(`${pathname}?${params.toString()}`);
+      }
+    },
+    [router, pathname, searchParams],
+  );
+
+  // Side effect: update URL if we're in a demo environment and URL parameter is
+  // missing or is invalid.
+  useEffect(() => {
+    if (!parseVariantResult.success && isLocalOrDemo) {
+      changeDemoFormVariantViaParam(DEFAULT_DEMO_FORM_VARIANT, {
+        replaceUrl: true,
+      });
     }
-  }
+  }, [parseVariantResult.success, changeDemoFormVariantViaParam]);
 
   return {
     activeFormVariant,
